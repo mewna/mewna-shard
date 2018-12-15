1497f9edcacfbd9d8acccedb88cbe4a8dedfc2b4
amy null <null@amy.gg>
2018-12-15 01:27:04 -0500

    Refactoring

--- src/main/java/com/mewna/discord/shard/MewnaShard.java
+++ src/main/java/com/mewna/discord/shard/MewnaShard.java
@@ -178,6 +178,7 @@ public final class MewnaShard {
     }
     
     private void startSharding() {
+        lighthouse.release();
         c(lighthouse.service().lock())
                 .thenAccept(lock -> {
                     if(lock) {
