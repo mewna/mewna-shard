ce6e0d377cf8162aedfed15a8290d4fef82fc67d
amy null <null@amy.gg>
2018-12-26 04:13:46 -0500

    Fix counting being hard

--- src/main/java/com/mewna/discord/shard/EventInspectorExtension.java
+++ src/main/java/com/mewna/discord/shard/EventInspectorExtension.java
@@ -41,7 +41,7 @@ class EventInspectorExtension extends AbstractExtension {
             @Override
             public JsonObject rawGatewayReceiveHook(@Nonnull final JsonObject json) {
                 final String type = json.getString("t");
-                mewnaShard.statsClient().gauge("gatewayEvents", 1, "type:" + type);
+                mewnaShard.statsClient().count("gatewayEvents", 1, "type:" + type);
                 return json;
             }
         });
