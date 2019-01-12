3cd89d322d8d2a87ef09860572c0c41800332e5a
amy null <null@amy.gg>
2019-01-12 18:32:33 -0500

    Cleanup

--- src/main/java/com/mewna/discord/shard/EventInspectorExtension.java
+++ src/main/java/com/mewna/discord/shard/EventInspectorExtension.java
@@ -2,11 +2,7 @@ package com.mewna.discord.shard;
 
 import com.mewna.catnip.extension.AbstractExtension;
 import com.mewna.catnip.extension.hook.CatnipHook;
-import com.mewna.catnip.shard.GatewayOp;
-import io.sentry.Sentry;
 import io.vertx.core.json.JsonObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 
@@ -15,16 +11,15 @@ import javax.annotation.Nonnull;
  * @since 12/13/18.
  */
 class EventInspectorExtension extends AbstractExtension {
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-    private MewnaShard mewnaShard;
+    private final MewnaShard mewnaShard;
     
-    public EventInspectorExtension(final MewnaShard mewnaShard) {
+    EventInspectorExtension(final MewnaShard mewnaShard) {
         super("event-inspector");
         this.mewnaShard = mewnaShard;
     }
     
     @Override
-    public void start() throws Exception {
+    public void start() {
         registerHook(new CatnipHook() {
             @Override
             public JsonObject rawGatewayReceiveHook(@Nonnull final JsonObject json) {
